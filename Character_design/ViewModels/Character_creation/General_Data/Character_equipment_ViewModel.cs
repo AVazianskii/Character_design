57042603833a28a0a0fb0f0256a5d822b06a7189
avazianskii@mail.ru <avazianskii@mail.ru>
2022-03-21 16:08:21 +0500

    added equipment page in character general info page

---
+++ Character_design/ViewModels/Character_creation/General_Data/Character_equipment_ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_design
+{
+    internal class Character_equipment_ViewModel : Notify
+    {
+        private static Character_equipment_ViewModel _instance;
+        public static Character_equipment_ViewModel GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Character_equipment_ViewModel();
+            }
+            return _instance;
+        }
+
+
+        private Character_equipment_ViewModel()
+        {
+
+        }
+    }
+}
--- Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
@@ -12,6 +12,7 @@ namespace Character_design
         private Notify Character_skills;
         private Notify Character_forms;
         private Notify Character_features;
+        private Notify Character_equipment;
         private SolidColorBrush character_info_button_border;
         private SolidColorBrush character_skills_button_border;
         private SolidColorBrush Wheat_brush;
@@ -21,6 +22,7 @@ namespace Character_design
         public Command Open_character_skills { get; private set; }
         public Command Open_character_forms { get; private set; }
         public Command Open_character_features { get; private set; }
+        public Command Open_character_equipment { get; private set; }
 
         public static General_Data_ViewModel GetInstance()
         {
@@ -54,12 +56,14 @@ namespace Character_design
             Character_skills    = Character_skills_ViewModel.GetInstance();
             Character_forms     = Character_forms_ViewModel.GetInstance();
             Character_features  = Character_features_ViewModel.GetInstance();
+            Character_equipment = Character_equipment_ViewModel.GetInstance();
             currentViewModel = null;
 
-            Open_character_info     = new Command(o => _Open_character_info());
-            Open_character_skills   = new Command(o => _Open_character_skills());
-            Open_character_forms    = new Command(o => _Open_character_forms());
-            Open_character_features = new Command(o => _Open_character_features());
+            Open_character_info         = new Command(o => _Open_character_info());
+            Open_character_skills       = new Command(o => _Open_character_skills());
+            Open_character_forms        = new Command(o => _Open_character_forms());
+            Open_character_features     = new Command(o => _Open_character_features());
+            Open_character_equipment    = new Command(o => _Open_character_equipment());
 
             Black_brush = new SolidColorBrush(Colors.Black);
             Wheat_brush = new SolidColorBrush(Colors.Wheat);
@@ -82,6 +86,10 @@ namespace Character_design
         private void _Open_character_features()
         {
             CurrentViewModel = Character_features;
+        }
+        private void _Open_character_equipment()
+        {
+            CurrentViewModel = Character_equipment;
         }
     }
 }
