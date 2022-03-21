8117e908bfa648ff89ac22ea23393e60f59d9eba
avazianskii@mail.ru <avazianskii@mail.ru>
2022-03-21 15:54:48 +0500

    added CHaracter_features page in charatcer general info page

--- Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
@@ -11,6 +11,7 @@ namespace Character_design
         private Notify Character_info;
         private Notify Character_skills;
         private Notify Character_forms;
+        private Notify Character_features;
         private SolidColorBrush character_info_button_border;
         private SolidColorBrush character_skills_button_border;
         private SolidColorBrush Wheat_brush;
@@ -19,6 +20,7 @@ namespace Character_design
         public Command Open_character_info { get; private set; }
         public Command Open_character_skills { get; private set; }
         public Command Open_character_forms { get; private set; }
+        public Command Open_character_features { get; private set; }
 
         public static General_Data_ViewModel GetInstance()
         {
@@ -48,14 +50,16 @@ namespace Character_design
 
         private General_Data_ViewModel()
         {
-            Character_info = Character_info_ViewModel.GetInstance();
-            Character_skills = Character_skills_ViewModel.GetInstance();
-            Character_forms = Character_forms_ViewModel.GetInstance();
+            Character_info      = Character_info_ViewModel.GetInstance();
+            Character_skills    = Character_skills_ViewModel.GetInstance();
+            Character_forms     = Character_forms_ViewModel.GetInstance();
+            Character_features  = Character_features_ViewModel.GetInstance();
             currentViewModel = null;
 
-            Open_character_info = new Command(o => _Open_character_info());
-            Open_character_skills = new Command(o => _Open_character_skills());
-            Open_character_forms = new Command(o => _Open_character_forms());
+            Open_character_info     = new Command(o => _Open_character_info());
+            Open_character_skills   = new Command(o => _Open_character_skills());
+            Open_character_forms    = new Command(o => _Open_character_forms());
+            Open_character_features = new Command(o => _Open_character_features());
 
             Black_brush = new SolidColorBrush(Colors.Black);
             Wheat_brush = new SolidColorBrush(Colors.Wheat);
@@ -74,6 +78,10 @@ namespace Character_design
         private void _Open_character_forms()
         {
             CurrentViewModel = Character_forms;
+        }
+        private void _Open_character_features()
+        {
+            CurrentViewModel = Character_features;
         }
     }
 }
