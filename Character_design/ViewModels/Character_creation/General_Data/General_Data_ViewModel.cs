439dc2890143a97ff0fc6ebbe4e65a1623f3b3e9
avazianskii@mail.ru <avazianskii@mail.ru>
2022-03-21 14:52:41 +0500

    tried to make dynamic coloring border of buttons

--- Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Character_design
 {
     internal class General_Data_ViewModel : Notify
     {
         private static General_Data_ViewModel _instance;
+        private Notify currentViewModel;
+        private SolidColorBrush character_button_border;
+        private SolidColorBrush Wheat_brush;
+        private SolidColorBrush Black_brush;
+        
+        public Command Open_character_info { get; private set; }
+
         public static General_Data_ViewModel GetInstance()
         {
             if (_instance == null)
@@ -16,6 +21,34 @@ namespace Character_design
                 _instance = new General_Data_ViewModel();
             }
             return _instance;
+        }
+
+
+        public Notify CurrentViewModel
+        {
+            get { return currentViewModel; }
+            set { currentViewModel = value; OnPropertyChanged("CurrentViewModel"); }
+        }
+        public SolidColorBrush Character_button_border
+        {
+            get { return character_button_border;  }
+            set { character_button_border = value; OnPropertyChanged("Character_button_border"); }
+        }
+
+        private General_Data_ViewModel()
+        {
+            currentViewModel = null;
+
+            Open_character_info = new Command(o => _Open_character_info());
+
+            Black_brush = new SolidColorBrush(Colors.Black);
+            Wheat_brush = new SolidColorBrush(Colors.Wheat);
+            Character_button_border = Wheat_brush;
+        }
+
+        private void _Open_character_info()
+        {
+            Character_button_border = Wheat_brush;
         }
     }
 }
