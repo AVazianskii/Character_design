659ca74c5a53e9a93802592e7eb955b2f064cc0e
avazianskii@mail.ru <avazianskii@mail.ru>
2022-03-21 18:22:13 +0300

    initial GUI for character general info

--- Character_design/ViewModels/Character_creation/General_Data/Character_info_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/Character_info_ViewModel.cs
@@ -9,6 +9,21 @@ namespace Character_design
     internal class Character_info_ViewModel : Notify
     {
         private static Character_info_ViewModel _instance;
+
+        private string character_name;
+        private string character_race_name;
+
+        public string Character_name
+        {
+            get { return character_name; }
+            set { character_name = value; OnPropertyChanged("Character_name"); }
+        }
+        public string Character_race_name
+        {
+            get { character_race_name = Character.GetInstance().Character_race.Race_name; return character_race_name; }
+            set { character_race_name = value; OnPropertyChanged("Character_race_name"); }
+        }
+
         public static Character_info_ViewModel GetInstance()
         {
             if (_instance == null)
@@ -21,7 +36,7 @@ namespace Character_design
 
         private Character_info_ViewModel()
         {
-
+            Character_name = "Дарт Сидиус";
         }
     }
 }
