25cc0fe2039df77968a3235e450f0ecf117a19b4
avazianskii@mail.ru <avazianskii@mail.ru>
2022-03-21 16:22:54 +0500

    added spaceship page in character general info

--- Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/General_Data_ViewModel.cs
@@ -13,6 +13,7 @@ namespace Character_design
         private Notify Character_forms;
         private Notify Character_features;
         private Notify Character_equipment;
+        private Notify Character_spaceship;
         private SolidColorBrush character_info_button_border;
         private SolidColorBrush character_skills_button_border;
         private SolidColorBrush Wheat_brush;
@@ -23,6 +24,7 @@ namespace Character_design
         public Command Open_character_forms { get; private set; }
         public Command Open_character_features { get; private set; }
         public Command Open_character_equipment { get; private set; }
+        public Command Open_character_spaceship { get; private set; }
 
         public static General_Data_ViewModel GetInstance()
         {
@@ -57,6 +59,7 @@ namespace Character_design
             Character_forms     = Character_forms_ViewModel.GetInstance();
             Character_features  = Character_features_ViewModel.GetInstance();
             Character_equipment = Character_equipment_ViewModel.GetInstance();
+            Character_spaceship = Character_spaceship_ViewModel.GetInstance();
             currentViewModel = null;
 
             Open_character_info         = new Command(o => _Open_character_info());
@@ -64,6 +67,7 @@ namespace Character_design
             Open_character_forms        = new Command(o => _Open_character_forms());
             Open_character_features     = new Command(o => _Open_character_features());
             Open_character_equipment    = new Command(o => _Open_character_equipment());
+            Open_character_spaceship    = new Command(o => _Open_character_spaceship());       
 
             Black_brush = new SolidColorBrush(Colors.Black);
             Wheat_brush = new SolidColorBrush(Colors.Wheat);
@@ -90,6 +94,10 @@ namespace Character_design
         private void _Open_character_equipment()
         {
             CurrentViewModel = Character_equipment;
+        }
+        private void _Open_character_spaceship()
+        {
+            CurrentViewModel = Character_spaceship;
         }
     }
 }
