a0045f140e1c8cb798062d046ca188bf23eb3f00
AVazianskii <avazianskii@mail.ru>
2022-03-23 09:56:46 +0300

    added new method in abstraction class. Class renaming is needed

--- Character_design/MVVM infrastructure/Notify.cs
+++ Character_design/MVVM infrastructure/Notify.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Windows.Media;
 
 namespace Character_design
 {
@@ -10,6 +12,24 @@ namespace Character_design
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void Set_colors_for_chosen_item (List<SolidColorBrush> SolidBrushes, 
+                                                SolidColorBrush Need_brush,
+                                                Color Chosen_color,
+                                                Color Unchosen_color)
+        {
+            foreach (var item in SolidBrushes)
+            {
+                if (item == Need_brush)
+                {
+                    Need_brush.Color = Chosen_color;
+                }
+                else
+                {
+                    item.Color = Unchosen_color;
+                }
+            }
         }
     }
 }
