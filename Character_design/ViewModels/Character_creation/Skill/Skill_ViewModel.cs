3aaae70817a82a730d060549b276d0b4af1822ca
avazianskii@mail.ru <avazianskii@mail.ru>
2022-04-23 19:05:26 +0500

    some code refactoring

--- Character_design/ViewModels/Character_creation/Skill/Skill_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/Skill/Skill_ViewModel.cs
@@ -45,53 +45,23 @@ namespace Character_design
 
         private int selected_skill_limit;
 
-        private int Return_skill_cost(Skill_Class skill)
-        {
-            if(Character.GetInstance().Forceuser)
-            {
-                return skill.Get_Forceuser_cost();
-            }
-            else
-            {
-                return skill.Get_Non_force_user_cost();
-            }
-        }
-        private int Return_skill_limit(Skill_Class skill, Age_status_libs.Age_status_class age_status, Range_libs.Range_Class range)
-        {
-            int result = 0;
-            switch (skill.Skill_type)
-            {
-                case 1:
-                    if(age_status.Skill_limit <= range.Combat_skill_limit) { result = age_status.Skill_limit; }
-                    else { result = range.Combat_skill_limit; }
-                    break;
-                case 2:
-                    if (age_status.Skill_limit <= range.Surviving_skill_limit) { result = age_status.Skill_limit; }
-                    else { result = range.Surviving_skill_limit; }
-                    break;
-                case 3:
-                    if (age_status.Skill_limit <= range.Charming_skill_limit) { result = age_status.Skill_limit; }
-                    else { result = range.Charming_skill_limit; }
-                    break;
-                case 4:
-                    if (age_status.Skill_limit <= range.Tech_skill_limit) { result = age_status.Skill_limit; }
-                    else { result = range.Tech_skill_limit; }
-                    break;
-                case 5:
-                    if (age_status.Skill_limit <= range.Specific_skill_limit) { result = age_status.Skill_limit; }
-                    else { result = range.Specific_skill_limit; }
-                    break;
-            }
-            return result;
-        }
+
+
         public static Skill_ViewModel GetInstance()
         {
-            if (_instance == null)
-            {
-                _instance = new Skill_ViewModel();
-            }
+            if (_instance == null) { _instance = new Skill_ViewModel(); }
             return _instance;
         }
+
+
+
+        public Command Show_combat_skills { get; private set; }
+        public Command Show_surviving_skills { get; private set; }
+        public Command Show_charming_skills { get; private set; }
+        public Command Show_tech_skills { get; private set; }
+        public Command Show_specific_skills { get; private set; }
+        public Command Decrease_skill_score { get; private set; }
+        public Command Increase_skill_score { get; private set; }
 
 
 
@@ -125,126 +95,6 @@ namespace Character_design
             get { return specific_skills_button_border; }
             set { specific_skills_button_border = value; OnPropertyChanged("Specific_skills_button_border"); }
         }
-
-
-
-        public Command Show_combat_skills { get; private set; }
-        public Command Show_surviving_skills { get; private set; }
-        public Command Show_charming_skills { get; private set; }
-        public Command Show_tech_skills { get; private set; }
-        public Command Show_specific_skills { get; private set; }
-        public Command Decrease_skill_score { get; private set; }
-        public Command Increase_skill_score { get; private set; }
-
-
-
-        private Skill_ViewModel()
-        {
-            combat_skills       = Skill_manager.GetInstance().Get_Combat_skills();
-            surviving_skills    = Skill_manager.GetInstance().Get_Survivng_skills();
-            charming_skills     = Skill_manager.GetInstance().Get_Charming_skills();
-            tech_skills         = Skill_manager.GetInstance().Get_Tech_skills();
-            specific_skills     = Skill_manager.GetInstance().Get_Specific_skills();
-
-            skill_group     = combat_skills;
-            Selected_skill  = skill_group[0];
-
-            Show_combat_skills      = new Command(o => _Show_combat_skills());
-            Show_surviving_skills   = new Command(o => _Show_surviving_skills());
-            Show_charming_skills    = new Command(o => _Show_charming_skills());
-            Show_tech_skills        = new Command(o => _Show_tech_skills());
-            Show_specific_skills    = new Command(o => _Show_specific_skills());
-
-            Decrease_skill_score = new Command(o => _Decrease_skill_score(o), o => selected_skill.Get_counter() > 0);
-            Increase_skill_score = new Command(o => _Increase_skill_score(o), o => selected_skill.Get_counter() < Selected_skill_limit);
-
-            Combat_skills_button_border     = new SolidColorBrush();
-            Surviving_skills_button_border  = new SolidColorBrush();
-            Charming_skills_button_border   = new SolidColorBrush();
-            Tech_skills_button_border       = new SolidColorBrush();
-            Specific_skills_button_border   = new SolidColorBrush();
-
-            Button_borders = new List<SolidColorBrush>();
-
-            Button_borders.Add(Combat_skills_button_border);
-            Button_borders.Add(Surviving_skills_button_border);
-            Button_borders.Add(Charming_skills_button_border);
-            Button_borders.Add(Tech_skills_button_border);
-            Button_borders.Add(Specific_skills_button_border);
-
-            Chosen_color = Colors.Wheat;
-            Unchosen_color = Colors.Black;
-
-            Set_colors_for_chosen_item(Button_borders,Combat_skills_button_border,Chosen_color,Unchosen_color);
-        }
-        private void _Show_combat_skills()
-        {
-            Skill_group = combat_skills;
-            Selected_skill = Skill_group[0];
-
-            Set_colors_for_chosen_item(Button_borders, Combat_skills_button_border, Chosen_color, Unchosen_color);
-        }
-        private void _Show_surviving_skills()
-        {
-            Skill_group = surviving_skills;
-            Selected_skill = Skill_group[0];
-
-            Set_colors_for_chosen_item(Button_borders, Surviving_skills_button_border, Chosen_color, Unchosen_color);
-        }
-        private void _Show_charming_skills()
-        {
-            Skill_group = charming_skills;
-            Selected_skill = Skill_group[0];
-
-            Set_colors_for_chosen_item(Button_borders, Charming_skills_button_border, Chosen_color, Unchosen_color);
-        }
-        private void _Show_tech_skills()
-        {
-            Skill_group = tech_skills;
-            Selected_skill = Skill_group[0];
-
-            Set_colors_for_chosen_item(Button_borders, Tech_skills_button_border, Chosen_color, Unchosen_color);
-        }
-        private void _Show_specific_skills()
-        {
-            Skill_group = specific_skills;
-            Selected_skill = Skill_group[0];
-
-            Set_colors_for_chosen_item(Button_borders, Specific_skills_button_border, Chosen_color, Unchosen_color);
-        }
-        private void _Increase_skill_score(object o)
-        {
-            Skill_Class skill = o as Skill_Class;
-            if (skill != null)
-            {
-                foreach (Skill_Class Character_skill in Character.GetInstance().Get_skills())
-                {
-                    if (skill.Get_skill_name() == Character_skill.Get_skill_name())
-                    {
-                        Character_skill.Increase_score();
-                        Selected_skill_score = Character_skill.Get_score();
-                        break;
-                    }
-                }
-            }
-        }
-        private void _Decrease_skill_score(object o)
-        {
-            Skill_Class skill = o as Skill_Class;
-            if (skill != null)
-            {
-                foreach (Skill_Class Character_skill in Character.GetInstance().Get_skills())
-                {
-                    if (skill.Get_skill_name() == Character_skill.Get_skill_name())
-                    {
-                        Character_skill.Decrease_score();
-                        Selected_skill_score = Character_skill.Get_score();
-                        break;
-                    }
-                }
-            }
-        }
-
         public int Exp_points_left
         {
             get
@@ -270,10 +120,10 @@ namespace Character_design
                     Selected_skill_title = selected_skill.Get_skill_name();
                     Selected_skill_score = selected_skill.Get_score();
                     // TODO: сделать логику определения максимального значения навыка, опираясь на возраст, ранг и прочее
-                    Selected_skill_max_score = 10; 
+                    Selected_skill_max_score = 10;
                     Selected_skill_race_bonus = 0;
                     // TODO: сделать логику определения минимального значения навыка, опираясь на расовые бонусы и прочее
-                    Selected_skill_min_score = Selected_skill_race_bonus;  
+                    Selected_skill_min_score = Selected_skill_race_bonus;
                     Selected_skill_range_limit = selected_skill.Get_range_skill_limit();
                     selected_skill_age_limit = selected_skill.Get_age_skill_limit();
                     Selected_skill_cost = Return_skill_cost(selected_skill);
@@ -341,6 +191,157 @@ namespace Character_design
         {
             get { return selected_skill_limit; }
             set { selected_skill_limit = value; OnPropertyChanged("Selected_skill_limit"); }
+        }
+
+
+
+        private Skill_ViewModel()
+        {
+            combat_skills       = Skill_manager.GetInstance().Get_Combat_skills();
+            surviving_skills    = Skill_manager.GetInstance().Get_Survivng_skills();
+            charming_skills     = Skill_manager.GetInstance().Get_Charming_skills();
+            tech_skills         = Skill_manager.GetInstance().Get_Tech_skills();
+            specific_skills     = Skill_manager.GetInstance().Get_Specific_skills();
+
+            skill_group     = combat_skills;
+            Selected_skill  = skill_group[0];
+
+            Show_combat_skills      = new Command(o => _Show_combat_skills());
+            Show_surviving_skills   = new Command(o => _Show_surviving_skills());
+            Show_charming_skills    = new Command(o => _Show_charming_skills());
+            Show_tech_skills        = new Command(o => _Show_tech_skills());
+            Show_specific_skills    = new Command(o => _Show_specific_skills());
+
+            Decrease_skill_score = new Command(o => _Decrease_skill_score(o), o => selected_skill.Get_counter() > 0);
+            Increase_skill_score = new Command(o => _Increase_skill_score(o), o => selected_skill.Get_counter() < Selected_skill_limit);
+
+            Combat_skills_button_border     = new SolidColorBrush();
+            Surviving_skills_button_border  = new SolidColorBrush();
+            Charming_skills_button_border   = new SolidColorBrush();
+            Tech_skills_button_border       = new SolidColorBrush();
+            Specific_skills_button_border   = new SolidColorBrush();
+
+            Button_borders = new List<SolidColorBrush>();
+
+            Button_borders.Add(Combat_skills_button_border);
+            Button_borders.Add(Surviving_skills_button_border);
+            Button_borders.Add(Charming_skills_button_border);
+            Button_borders.Add(Tech_skills_button_border);
+            Button_borders.Add(Specific_skills_button_border);
+
+            Chosen_color = Colors.Wheat;
+            Unchosen_color = Colors.Black;
+
+            Set_colors_for_chosen_item(Button_borders,Combat_skills_button_border,Chosen_color,Unchosen_color);
+        }
+
+
+        
+        private int Return_skill_cost(Skill_Class skill)
+        {
+            if (Character.GetInstance().Forceuser)
+            {
+                return skill.Get_Forceuser_cost();
+            }
+            else
+            {
+                return skill.Get_Non_force_user_cost();
+            }
+        }
+        private int Return_skill_limit(Skill_Class skill, Age_status_libs.Age_status_class age_status, Range_libs.Range_Class range)
+        {
+            int result = 0;
+            switch (skill.Skill_type)
+            {
+                case 1:
+                    if (age_status.Skill_limit <= range.Combat_skill_limit) { result = age_status.Skill_limit; }
+                    else { result = range.Combat_skill_limit; }
+                    break;
+                case 2:
+                    if (age_status.Skill_limit <= range.Surviving_skill_limit) { result = age_status.Skill_limit; }
+                    else { result = range.Surviving_skill_limit; }
+                    break;
+                case 3:
+                    if (age_status.Skill_limit <= range.Charming_skill_limit) { result = age_status.Skill_limit; }
+                    else { result = range.Charming_skill_limit; }
+                    break;
+                case 4:
+                    if (age_status.Skill_limit <= range.Tech_skill_limit) { result = age_status.Skill_limit; }
+                    else { result = range.Tech_skill_limit; }
+                    break;
+                case 5:
+                    if (age_status.Skill_limit <= range.Specific_skill_limit) { result = age_status.Skill_limit; }
+                    else { result = range.Specific_skill_limit; }
+                    break;
+            }
+            return result;
+        }
+        private void _Show_combat_skills()
+        {
+            Skill_group = combat_skills;
+            Selected_skill = Skill_group[0];
+
+            Set_colors_for_chosen_item(Button_borders, Combat_skills_button_border, Chosen_color, Unchosen_color);
+        }
+        private void _Show_surviving_skills()
+        {
+            Skill_group = surviving_skills;
+            Selected_skill = Skill_group[0];
+
+            Set_colors_for_chosen_item(Button_borders, Surviving_skills_button_border, Chosen_color, Unchosen_color);
+        }
+        private void _Show_charming_skills()
+        {
+            Skill_group = charming_skills;
+            Selected_skill = Skill_group[0];
+
+            Set_colors_for_chosen_item(Button_borders, Charming_skills_button_border, Chosen_color, Unchosen_color);
+        }
+        private void _Show_tech_skills()
+        {
+            Skill_group = tech_skills;
+            Selected_skill = Skill_group[0];
+
+            Set_colors_for_chosen_item(Button_borders, Tech_skills_button_border, Chosen_color, Unchosen_color);
+        }
+        private void _Show_specific_skills()
+        {
+            Skill_group = specific_skills;
+            Selected_skill = Skill_group[0];
+
+            Set_colors_for_chosen_item(Button_borders, Specific_skills_button_border, Chosen_color, Unchosen_color);
+        }
+        private void _Increase_skill_score(object o)
+        {
+            Skill_Class skill = o as Skill_Class;
+            if (skill != null)
+            {
+                foreach (Skill_Class Character_skill in Character.GetInstance().Get_skills())
+                {
+                    if (skill.Get_skill_name() == Character_skill.Get_skill_name())
+                    {
+                        Character_skill.Increase_score();
+                        Selected_skill_score = Character_skill.Get_score();
+                        break;
+                    }
+                }
+            }
+        }
+        private void _Decrease_skill_score(object o)
+        {
+            Skill_Class skill = o as Skill_Class;
+            if (skill != null)
+            {
+                foreach (Skill_Class Character_skill in Character.GetInstance().Get_skills())
+                {
+                    if (skill.Get_skill_name() == Character_skill.Get_skill_name())
+                    {
+                        Character_skill.Decrease_score();
+                        Selected_skill_score = Character_skill.Get_score();
+                        break;
+                    }
+                }
+            }
         }
     }
 }
