80e7d73e59f9424fce1f59d66e4c44a12c44ccad
AVazianskii <avazianskii@mail.ru>
2022-05-12 21:17:16 +0300

    added new Commanf template for command, which change character state

---
+++ Character_design/MVVM infrastructure/Character_changing_command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Character_design
+{
+    public class Character_changing_command : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Character_changing_command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+            Character.GetInstance().Change_character_state_to_unsave();
+        }
+
+    }
+}
