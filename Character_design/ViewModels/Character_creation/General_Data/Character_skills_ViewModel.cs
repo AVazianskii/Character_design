f3af282b53c76f96d0f41d6bd800e1ad14388aba
AVazianskii <avazianskii@mail.ru>
2022-06-08 00:29:33 +0300

    added some elements in character skills page

--- Character_design/ViewModels/Character_creation/General_Data/Character_skills_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/Character_skills_ViewModel.cs
@@ -9,7 +9,32 @@ namespace Character_design
     {
         private static Character_skills_ViewModel _instance;
 
-        private List<Skill_Class> skill_group;
+        private object skill_group;
+        private object selected_skill;
+        private List<Skill_Class> usual_skills_group;
+        private List<Force_skill_class> force_skills_group;
+
+        private int button_opacity;
+
+
+
+        public Command Show_skills { get; private set; }
+        public Command Show_force_skills { get; private set; }
+        public object Skill_group
+        {
+            get { return skill_group; }
+        }
+        public object Selected_skill
+        {
+            get { return selected_skill; }
+            set { selected_skill = value; OnPropertyChanged("Selected_skill"); }
+        }
+        public int Button_opacity
+        {
+            get { return button_opacity; }
+            set { button_opacity = value; OnPropertyChanged("Button_opacity"); }
+        }
+
 
 
         public static Character_skills_ViewModel GetInstance()
@@ -22,9 +47,27 @@ namespace Character_design
         }
 
 
+
         private Character_skills_ViewModel()
         {
+            skill_group = new object();
+            usual_skills_group = Character.GetInstance().Skills_with_points;
+            //force_skills_group = Character.GetInstance().Force_skills_with_points;
 
+            Show_skills = new Command(o => _Show_skills());
+
+            Button_opacity = 100;
+        }
+
+
+
+        private void _Show_skills()
+        {
+            skill_group = usual_skills_group;
+        }
+        private void _Show_force_skills()
+        {
+            skill_group = force_skills_group;
         }
     }
 }
