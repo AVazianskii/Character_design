a8fef6a89b93f3fd6e0b3eeacaed8a3b0ff769e6
AVazianskii <avazianskii@mail.ru>
2022-06-10 00:00:59 +0300

    added animation for borders in button click

--- Character_design/ViewModels/Character_creation/General_Data/Character_skills_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/General_Data/Character_skills_ViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Skills_libs;
 using SW_Character_creation;
+using System.Windows.Media;
 
 namespace Character_design
 {
@@ -13,6 +14,14 @@ namespace Character_design
         private object selected_skill;
         private List<Skill_Class> usual_skills_group;
         private List<Force_skill_class> force_skills_group;
+
+        private SolidColorBrush skills_border,
+                                force_skills_border;
+
+        private List<SolidColorBrush> Button_borders;
+
+        private Color Chosen_color,
+                      Unchosen_color;
 
         private int button_opacity;
 
@@ -34,6 +43,16 @@ namespace Character_design
             get { return button_opacity; }
             set { button_opacity = value; OnPropertyChanged("Button_opacity"); }
         }
+        public SolidColorBrush Skills_border
+        {
+            get { return skills_border; }
+            set { skills_border = value; OnPropertyChanged("Skills_border"); }
+        }
+        public SolidColorBrush Force_skills_border
+        {
+            get { return force_skills_border; }
+            set { force_skills_border = value; OnPropertyChanged("Force_skills_border"); }
+        }
 
 
 
@@ -54,9 +73,22 @@ namespace Character_design
             usual_skills_group = Character.GetInstance().Skills_with_points;
             //force_skills_group = Character.GetInstance().Force_skills_with_points;
 
-            Show_skills = new Command(o => _Show_skills());
+            Skills_border       = new SolidColorBrush();
+            Force_skills_border = new SolidColorBrush();
+
+            Button_borders = new List<SolidColorBrush>();
+            Button_borders.Add(Skills_border);
+            Button_borders.Add(Force_skills_border);
+
+            Show_skills         = new Command(o => _Show_skills());
+            Show_force_skills   = new Command(o => _Show_force_skills());
+
+            Chosen_color    = Colors.Wheat;
+            Unchosen_color  = Colors.Black;
 
             Button_opacity = 100;
+
+            Set_colors_for_chosen_item(Button_borders, Skills_border, Chosen_color, Unchosen_color);
         }
 
 
@@ -64,10 +96,14 @@ namespace Character_design
         private void _Show_skills()
         {
             skill_group = usual_skills_group;
+
+            Set_colors_for_chosen_item(Button_borders, Skills_border, Chosen_color, Unchosen_color);
         }
         private void _Show_force_skills()
         {
-            skill_group = force_skills_group;
+            //skill_group = force_skills_group;
+
+            Set_colors_for_chosen_item(Button_borders, Force_skills_border, Chosen_color, Unchosen_color);
         }
     }
 }
