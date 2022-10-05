f721011d2daeac462fba60fcdbe1819e9235834b
AVazianskii <avazianskii@mail.ru>
2022-10-05 22:25:48 +0300

    added some new folders and classes for common views

---
+++ Character_design/ViewModels/Character_creation/Common_VViewModels/Nothing_chosen_ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_design
+{
+    internal class Nothing_chosen_ViewModel : BaseViewModel
+    {
+        private static Nothing_chosen_ViewModel _instance;
+
+
+        public static Nothing_chosen_ViewModel GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Nothing_chosen_ViewModel();
+            }
+            return _instance;
+        }
+
+
+        private Nothing_chosen_ViewModel()
+        {
+
+        }
+    }
+}
