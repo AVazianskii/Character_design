ecedbde3290eae730fca37bf8d710f56058aae54
AVazianskii <avazianskii@mail.ru>
2022-10-27 22:18:47 +0300

    added some code and GUI for features page of character

--- Character_design/ViewModels/Character_creation/Features/Features_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/Features/Features_ViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using SW_Character_creation;
+using System.Windows.Media;
 using System.IO;
 
 namespace Character_design
@@ -7,11 +9,39 @@ namespace Character_design
     internal class Features_ViewModel : BaseViewModel
     {
         private static Features_ViewModel _instance;
+        private SolidColorBrush positive_feature_border,
+                                negative_feature_border;
+
+        private List<SolidColorBrush> Button_borders;
+
+        private int num_skills_left;
+
+        private Color Chosen_color,
+                      Unchosen_color;
 
 
-        public string Img_path
+
+        public Command Show_positive_features { get; private set; }
+        public Command Show_negative_features { get; private set; }
+        public SolidColorBrush Positive_feature_border
         {
-            get { return $@"{Directory.GetCurrentDirectory()}\Pictures\Common\Content_soon.jpg"; }
+            get { return positive_feature_border; }
+            set { positive_feature_border = value; OnPropertyChanged("Positive_feature_border"); }
+        }
+        public SolidColorBrush Negative_feature_border
+        {
+            get { return negative_feature_border; }
+            set { negative_feature_border = value; OnPropertyChanged("Negative_feature_border"); }
+        }
+        public int Num_skills_left
+        {
+            get { return num_skills_left; }
+            set { num_skills_left = value; OnPropertyChanged("Num_skills_left"); }
+        }
+        public int Exp_points_left
+        {
+            //TODO: сделать логику выбора особенностей персонажа
+            get { return 10; }
         }
 
 
@@ -28,7 +58,34 @@ namespace Character_design
 
         private Features_ViewModel()
         {
+            Show_positive_features = new Command(o => _Show_positive_features());
+            Show_negative_features = new Command(o => _Show_negative_features());
 
+            Positive_feature_border = new SolidColorBrush();
+            Negative_feature_border = new SolidColorBrush();
+
+            Button_borders = new List<SolidColorBrush>();
+            Button_borders.Add(Positive_feature_border);
+            Button_borders.Add(Negative_feature_border);
+
+            Chosen_color = Colors.Wheat;
+            Unchosen_color = Colors.Black;
+
+            Num_skills_left = Character.GetInstance().Limit_positive_features_left;
+            Set_colors_for_chosen_item(Button_borders, Positive_feature_border, Chosen_color, Unchosen_color);
+        }
+
+
+
+        private void _Show_positive_features()
+        {
+            Num_skills_left = Character.GetInstance().Limit_positive_features_left;
+            Set_colors_for_chosen_item(Button_borders, Positive_feature_border, Chosen_color, Unchosen_color);
+        }
+        private void _Show_negative_features()
+        {
+            Num_skills_left = Character.GetInstance().Limit_negative_features_left;
+            Set_colors_for_chosen_item(Button_borders, Negative_feature_border, Chosen_color, Unchosen_color);
         }
     }
 }
