bc25987f565a9bccf25373794f70a8e79051ff1c
AVazianskii <avazianskii@mail.ru>
2023-01-09 22:49:01 +0300

    added learning and deleting features mechanism

--- Character_design/ViewModels/Character_creation/Features/Features_ViewModel.cs
+++ Character_design/ViewModels/Character_creation/Features/Features_ViewModel.cs
@@ -30,7 +30,8 @@ namespace Character_design
                      textBlockOpacity;
 
         private bool comboBoxEnabled,
-                     textBlockEnabled;
+                     textBlockEnabled,
+                     negative_features_chosen;
 
         private string feature_choose_warning,
                        feature_choose_advice;
@@ -56,8 +57,17 @@ namespace Character_design
         }
         public int Num_skills_left
         {
-            get { return num_skills_left; }
-            set { num_skills_left = value; OnPropertyChanged("Num_skills_left"); }
+            get
+            {
+                if (current_feature_list == positive_features)
+                {
+                    return Character.GetInstance().Limit_positive_features_left;
+                }
+                else
+                {
+                    return Character.GetInstance().Limit_negative_features_left;
+                }
+            }
         }
         public List<All_feature_template> Current_feature_list
         {
@@ -81,13 +91,25 @@ namespace Character_design
                     OnPropertyChanged("Selected_feature_title");
                     OnPropertyChanged("Selected_feature_img_path");
                     OnPropertyChanged("Selected_feature_cost_list");
+                    OnPropertyChanged("Exp_points_left");
+                    OnPropertyChanged("Num_skills_left");
                 }
             }
         }
         public int Exp_points_left
         {
             //TODO: сделать логику выбора особенностей персонажа
-            get { return 10; }
+            get 
+            {   
+                if (current_feature_list == positive_features)
+                {
+                    return Character.GetInstance().Positive_features_points_left;
+                }
+                else
+                {
+                    return Character.GetInstance().Negative_features_points_left;
+                }
+            }
         }
         public string Question_sign
         {
@@ -186,6 +208,7 @@ namespace Character_design
             negative_features   = Feature_manager.GetInstance().Get_negative_features();
             
             Current_feature_list = positive_features;
+            negative_features_chosen = false;
 
             selected_feature = new All_feature_template();
 
@@ -197,7 +220,7 @@ namespace Character_design
             Chosen_color = Colors.Wheat;
             Unchosen_color = Colors.Black;
 
-            Num_skills_left = Character.GetInstance().Limit_positive_features_left;
+            //Num_skills_left = Character.GetInstance().Limit_positive_features_left;
             Set_colors_for_chosen_item(Button_borders, Positive_feature_border, Chosen_color, Unchosen_color);
         }
 
@@ -205,28 +228,43 @@ namespace Character_design
 
         private void _Show_positive_features()
         {
-            Num_skills_left = Character.GetInstance().Limit_positive_features_left;
+            //Num_skills_left = Character.GetInstance().Limit_positive_features_left;
             Set_colors_for_chosen_item(Button_borders, Positive_feature_border, Chosen_color, Unchosen_color);
 
             Current_feature_list = positive_features;
             Selected_feature = Current_feature_list[0];
             Select_show_cost(Selected_feature);
+            negative_features_chosen = false;
         }
         private void _Show_negative_features()
         {
-            Num_skills_left = Character.GetInstance().Limit_negative_features_left;
+            //Num_skills_left = Character.GetInstance().Limit_negative_features_left;
             Set_colors_for_chosen_item(Button_borders, Negative_feature_border, Chosen_color, Unchosen_color);
 
             Current_feature_list = negative_features;
             Selected_feature = Current_feature_list[0];
             Select_show_cost(Selected_feature);
+            negative_features_chosen = true;
         }
         private void _Learn_feature(object o)
         {
             All_feature_template feature = o as All_feature_template;
             if (feature != null)
             {
-                Character.GetInstance().Learn_feature(feature);
+                // ReferenceEquals - для сравнения ссылок на объекты
+                if (negative_features_chosen == false)
+                {
+                    Character.GetInstance().Spend_positive_feature_points(Selected_feature_cost);
+                    Character.GetInstance().Learn_positive_feature(feature);
+                }
+                else
+                {
+                    Character.GetInstance().Spend_negative_feature_points(Selected_feature_cost);
+                    Character.GetInstance().Learn_negative_feature(feature);
+                }
+                feature.Chosen_cost = Selected_feature_cost;
+                OnPropertyChanged("Exp_points_left");
+                OnPropertyChanged("Num_skills_left");
             }
         }
         private void _Delete_feature(object o)
@@ -234,12 +272,23 @@ namespace Character_design
             All_feature_template feature = o as All_feature_template;
             if (feature != null)
             {
-                Character.GetInstance().Delete_feature(feature);
+                if (negative_features_chosen == false)
+                {
+                    Character.GetInstance().Refund_positive_feature_points(feature.Chosen_cost);
+                    Character.GetInstance().Delete_positive_feature(feature);
+                }
+                else
+                {
+                    Character.GetInstance().Refund_negative_feature_points(feature.Chosen_cost);
+                    Character.GetInstance().Delete_negative_feature(feature);
+                }
+                OnPropertyChanged("Exp_points_left");
+                OnPropertyChanged("Num_skills_left");
             }
         }
         private bool Feature_learning_is_posible(All_feature_template feature)
         {
-            if (current_feature_list == positive_features)
+            if (negative_features_chosen == false)
             {
                 if (Selected_feature_cost > Character.GetInstance().Limit_positive_features_left)
                 {
@@ -248,7 +297,7 @@ namespace Character_design
                     return false;
                 }
             }
-            if (current_feature_list == negative_features)
+            if (negative_features_chosen == true)
             {
                 if (Selected_feature_cost > Character.GetInstance().Limit_negative_features_left)
                 {
@@ -269,7 +318,7 @@ namespace Character_design
         }
         private bool Feature_deleting_is_posible(All_feature_template feature)
         {
-            if (current_feature_list == positive_features)
+            if (negative_features_chosen == false)
             {
                 if(Character.GetInstance().Positive_features_with_points.Count == 0)
                 {
@@ -278,7 +327,7 @@ namespace Character_design
                     return false;
                 }
             }
-            if (current_feature_list == negative_features)
+            if (negative_features_chosen == true)
             {
                 if (Character.GetInstance().Negative_features_with_points.Count == 0)
                 {
