17fe5009b988cafae5925258fd38e4ba9d109dd9
AVazianskii <avazianskii@mail.ru>
2023-01-22 19:49:45 +0300

    Произведены некоторые графические перестановки на главной странице персонажа

--- Character_design/Properties/AssemblyInfo.cs
+++ Character_design/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.37.82")]
-[assembly: AssemblyFileVersion("2.0.37.82")]
+[assembly: AssemblyVersion("2.0.38.83")]
+[assembly: AssemblyFileVersion("2.0.38.83")]
